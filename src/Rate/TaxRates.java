909b7cba2badd395aa4981c6ee2a29eb3724e37a
elchinbaba <babalielchin@gmail.com>
2022-12-10 11:54:59 +0400

    update in TaxRates

--- src/Rate/TaxRates.java
+++ src/Rate/TaxRates.java
@@ -2,8 +2,8 @@ package Rate;
 
 public class TaxRates {
     public static double getTaxRate(int gross) {
-        if (gross < 700000) return 0.05;
-        if (gross <= 750000) return 0.08;
+        if (gross <= 700000) return 0.05;
+        if (gross < 750000) return 0.08;
         return 0.10;
     }
 }
